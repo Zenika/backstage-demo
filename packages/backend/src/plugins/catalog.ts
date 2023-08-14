9668dabfbacea95e95cfa1dbc423f3af23a436b9
Vincent LINAS <vincent.linas@zenika.com>
2023-08-14 15:29:04 +0200

    import users from github orga

--- packages/backend/src/plugins/catalog.ts
+++ packages/backend/src/plugins/catalog.ts
@@ -4,6 +4,18 @@ import { Router } from 'express';
 import { PluginEnvironment } from '../types';
 
 import { GitlabFillerProcessor } from '@immobiliarelabs/backstage-plugin-gitlab-backend';
+import { GithubOrgEntityProvider } from '@backstage/plugin-catalog-backend-module-github';
+import { Config } from '@backstage/config';
+
+interface IBackendConfig {
+  url: string;
+  schedule_frequency: number;
+  schedule_timeout: number;
+}
+
+const getOrgaConfig = (config: Config): IBackendConfig => {
+  return config.get<IBackendConfig>('backend.env.orga');
+};
 
 export default async function createPlugin(
   env: PluginEnvironment,
@@ -12,6 +24,18 @@ export default async function createPlugin(
   builder.addProcessor(new ScaffolderEntitiesProcessor());
 
   builder.addProcessor(new GitlabFillerProcessor(env.config));
+
+  builder.addEntityProvider(
+    GithubOrgEntityProvider.fromConfig(env.config, {
+      id: 'development',
+      orgUrl: getOrgaConfig(env.config).url,
+      logger: env.logger,
+      schedule: env.scheduler.createScheduledTaskRunner({
+        frequency: { hours: getOrgaConfig(env.config).schedule_frequency },
+        timeout: { minutes: getOrgaConfig(env.config).schedule_timeout },
+      }),
+    }),
+  );
 
   const { processingEngine, router } = await builder.build();
   await processingEngine.start();
